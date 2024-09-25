11bb1ab7b219bf2a863518cb566352d3bc6e01ca
Naimish Bhesaniya <102242463+Naimish-Dev@users.noreply.github.com>
2024-09-25 19:07:11 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -52,7 +52,21 @@ window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(argume
         </Script>
 
         <Script>{`gtag('config', 'AW-16561901636')`}</Script> */}
-      </head>
+     <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-16679540466"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-16679540466');
+            `,
+          }}
+        />
+            </head>
       <Conact />
       <body className={roboto.className}>{children}</body>
       <Footer />
